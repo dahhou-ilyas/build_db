a9f12a5c4e3b0716e6676bd39ce9ab1805f70ab7
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-12-09 22:13:47 +0100

    create struct of b-tree

---
+++ b-tree/bTreeImpl.go
@@ -0,0 +1,19 @@
+package b_tree
+
+type BNode struct {
+	data []byte // can be dumped to the disk
+}
+
+const (
+	BNODE_NODE = 1 // internal nodes without values
+	BNODE_LEAF = 2 // leaf nodes with values
+)
+
+type BTree struct {
+	// pointer (a nonzero page number)
+	root uint64
+	// callbacks for managing on-disk pages
+	get func(uint64) BNode // dereference a pointer
+	new func(BNode) uint64 // allocate a new page
+	del func(uint64)       // deallocate a page
+}
