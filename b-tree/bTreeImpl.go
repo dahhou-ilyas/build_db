15b75ad66711a194879dfe6872a8841dc6527d83
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-12-10 00:01:23 +0100

    handle pointer of page == node get and set

--- b-tree/bTreeImpl.go
+++ b-tree/bTreeImpl.go
@@ -49,3 +49,22 @@ func (node BNode) setHeader(btype uint16, nkeys uint16) {
 	binary.LittleEndian.PutUint16(node.data[0:2], btype)
 	binary.LittleEndian.PutUint16(node.data[2:4], nkeys)
 }
+
+// pointer
+
+func (node BNode) getPtr(idx uint16) uint64 {
+	if idx >= node.nkeys() {
+		panic("index out of range")
+	}
+	pos := HEADER + 8*idx
+
+	return binary.LittleEndian.Uint64(node.data[pos:])
+}
+
+func (node BNode) setPtr(idx uint16, val uint64) {
+	if idx >= node.nkeys() {
+		panic("index out of range")
+	}
+	pos := HEADER + 8*idx
+	binary.LittleEndian.PutUint64(node.data[pos:], val)
+}
