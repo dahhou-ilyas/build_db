491c9141aa16670d45faf908b6276319329701fc
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-12-09 23:10:07 +0100

    add page size verification

--- b-tree/bTreeImpl.go
+++ b-tree/bTreeImpl.go
@@ -17,3 +17,18 @@ type BTree struct {
 	new func(BNode) uint64 // allocate a new page
 	del func(uint64)       // deallocate a page
 }
+
+// page config
+
+const HEADER = 4
+const BTREE_PAGE_SIZE = 4096
+const BTREE_MAX_KEY_SIZE = 1000
+const BTREE_MAX_VAL_SIZE = 3000
+
+func init() {
+	node1max := HEADER + 8 + 2 + 4 + BTREE_MAX_KEY_SIZE + BTREE_MAX_VAL_SIZE
+
+	if node1max > BTREE_PAGE_SIZE {
+		panic("La taille maximale du nœud dépasse la taille de la page.")
+	}
+}
