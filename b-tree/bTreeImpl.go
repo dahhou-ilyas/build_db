c4a8432c54f9355af9cdf6f493db927b921cfc67
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-12-10 00:57:23 +0100

    manage offset

--- b-tree/bTreeImpl.go
+++ b-tree/bTreeImpl.go
@@ -68,3 +68,22 @@ func (node BNode) setPtr(idx uint16, val uint64) {
 	pos := HEADER + 8*idx
 	binary.LittleEndian.PutUint64(node.data[pos:], val)
 }
+
+// offset list
+func offsetPos(node BNode, idx uint16) uint16 {
+	if !(1 <= idx && idx <= node.nkeys()) {
+		panic("index out of bounds")
+	}
+	return HEADER + 8*node.nkeys() + 2*(idx-1)
+}
+
+func (node BNode) getOffset(idx uint16) uint16 {
+	if idx == 0 {
+		return 0
+	}
+	return binary.LittleEndian.Uint16(node.data[offsetPos(node, idx):])
+}
+
+func (node BNode) setOffset(idx uint16, offset uint16) {
+	binary.LittleEndian.PutUint16(node.data[offsetPos(node, idx):], offset)
+}
