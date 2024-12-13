6c23d934c48838a0492f334987b8951f68366406
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-12-13 20:34:58 +0100

    split node

--- b-tree/bTreeImpl.go
+++ b-tree/bTreeImpl.go
@@ -265,3 +265,36 @@ func nodeInsert(
 	// update the kid links
 	nodeReplaceKidN(tree, new, node, idx, splited[:nsplit]...)
 }
+
+// Split Big Nodes
+
+// split a bigger-than-allowed node into two.
+// the second node always fits on a page.
+
+func nodeSplit2(left BNode, right BNode, old BNode) {
+	// code omitted...
+}
+
+// split a node if it's too big. the results are 1~3 nodes.
+func nodeSplit3(old BNode) (uint16, [3]BNode) {
+	if old.nbytes() <= BTREE_PAGE_SIZE {
+		old.data = old.data[:BTREE_PAGE_SIZE]
+		return 1, [3]BNode{old}
+	}
+	left := BNode{make([]byte, 2*BTREE_PAGE_SIZE)} // might be split later
+	right := BNode{make([]byte, BTREE_PAGE_SIZE)}
+
+	nodeSplit2(left, right, old)
+	if left.nbytes() <= BTREE_PAGE_SIZE {
+		left.data = left.data[:BTREE_PAGE_SIZE]
+		return 2, [3]BNode{left, right}
+	}
+	// the left node is still too large
+	leftleft := BNode{make([]byte, BTREE_PAGE_SIZE)}
+	middle := BNode{make([]byte, BTREE_PAGE_SIZE)}
+	nodeSplit2(leftleft, middle, left)
+	if leftleft.nbytes() > BTREE_PAGE_SIZE {
+		panic("node bigger than page")
+	}
+	return 3, [3]BNode{leftleft, middle, right}
+}
